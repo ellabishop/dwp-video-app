51140885acb78dfb344858ce3f68185bab43839e
Peter Leon <peter.leon@engineering.digital.dwp.gov.uk>
2020-05-15 09:45:38 +0100

    New code validations

--- app/routes.js
+++ app/routes.js
@@ -73,15 +73,12 @@ router.post('/appointment-code-answer', function (req, res) {
 // Make a variable and give it the value from 'how-many-balls'
   var appointmentCode = req.session.data['appointmentCode']
 
-
-
 // Check whether the variable matches a condition
   if (appointmentCode == ""){
     // Send user to next page
     res.redirect('staff/join-appointment/code-error')
   }
   else {
-    // Send user to ineligible page
     res.redirect('staff/join-appointment/confirm')
   }
 
